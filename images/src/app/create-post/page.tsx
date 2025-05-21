b2a617b88b4626f8ec616f48c13308cafe8e1c23
Daniel Ding <ddin0018@student.monash.edu>
2025-05-21 17:25:10 +1000

    new image application using next js

---
+++ images/src/app/create-post/page.tsx
@@ -0,0 +1,8 @@
+export default function CreatePost(){
+    return (
+        <main className="text-center mt-10">
+            <h1>Create post</h1>
+            <p>TODO: create post form</p>
+        </main>
+    )
+}
